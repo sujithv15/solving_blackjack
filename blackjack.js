45bdd6ba2261ff6b0bf00ca61035ed6f052b6117
sujithv15 <64935098+sujithv15@users.noreply.github.com>
2022-11-14 22:14:39 -0500

    Initial commit with basic outline of game

---
+++ blackjack.js
@@ -0,0 +1,22 @@
+/*
+
+1. Create a deck, and shuffle cards
+
+2. Add desired amount of decks into the shoe
+
+3. Choose number of players (1-5?)
+
+4. Get the bankrolls straightened
+
+5. Start game:
+    a.  Deal cards, first dealer card face up, second face down
+    b.  Check around the table for blackjacks, and square up accordingly
+    c.  For remaining hands:
+        1.  Display the current score
+        2.  Give player option to hit or stay (or double or split if available)
+        3.  Execute player option, displaying his score and options, then repeat until stay/break
+    d.  For dealer's hand:
+        1.  Reveal hidden card, and hit or stay based on desired blackjack rules (hit/stay soft 16)
+    e.  Compare each player's hand with dealers, and settle payouts accordingly
+
+ */
